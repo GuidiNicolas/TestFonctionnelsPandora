b57a086476778c1c09ae2db3a030a3fb364764d3
guidinicolas <guidinicolas@gmail.com>
2016-12-15 00:58:19 +0100

    tests sur chrome

--- src/test/java/selenium/TestPOC.java
+++ src/test/java/selenium/TestPOC.java
@@ -1,14 +1,17 @@
 package selenium;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -28,11 +31,33 @@ public class TestPOC {
 
         System.out.println(driver.findElement(By.xpath("//h2")).toString());
 
-        assertTrue( driver.findElement(By.xpath("//h2")) != null);
+        try {
+            driver.findElement(By.xpath("//h2"));
+        }
+        catch (NoSuchElementException e) {
+            assertTrue(false);
+        }
+        assertTrue(true);
     }
 
-    @AfterClass
-    public static void killFirefox() {
+    @Test
+    public void testHibernateJSP2() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Apps\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 30);
+        driver.get("http://localhost:8088/HibernateJSP");
+
+        try {
+            driver.findElement(By.xpath("//h3"));
+        }
+        catch (NoSuchElementException e) {
+            assertEquals(true,false);
+        }
+        assertTrue(true);
+    }
+
+    @After
+    public void killFirefox() {
         driver.quit();
     }
 }
