de48811eab016e675abb6bd8d0dbee1b3d2d7e4a
guidinicolas <guidinicolas@gmail.com>
2016-12-15 01:04:50 +0100

    tests sur chome2

--- src/test/java/selenium/TestPOC.java
+++ src/test/java/selenium/TestPOC.java
@@ -1,13 +1,11 @@
 package selenium;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
